cfd8483d3d328cb978059bcbf1ef1905a63ecffb
conors-code <conor_darcy@hotmail.com>
2022-11-21 14:52:38 +0000

    Added Alchemy support. log delegator account.

--- scripts/DelegateVote.ts
+++ scripts/DelegateVote.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 import { Wallet } from "ethers";
-import { InfuraProvider } from "@ethersproject/providers";
+import { AlchemyProvider, InfuraProvider } from "@ethersproject/providers";
 import { Ballot, Ballot__factory, MyToken } from "../typechain-types";
 import { MyToken__factory } from "../typechain-types/factories/contracts/ERC20Votes.sol/MyToken__factory";
 dotenv.config();
@@ -11,9 +11,11 @@ async function main() {
   let proposals;
 
   //Deploy on Goerli accepting in an account address:
-  const provider = new InfuraProvider("goerli", process.env.INFURA_KEY);
+  //const provider = new InfuraProvider("goerli", process.env.INFURA_KEY);
+  const provider = new AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
   const wallet = new Wallet(process.env.PRIVATE_KEY ?? "");
   const signer = wallet.connect(provider);
+  console.log(`Signer (delegating from) address: ${signer.address}`);
   const contractAddress = params[0];
   const newDelegateAccount = params[1];
 
@@ -25,6 +27,7 @@ async function main() {
   const delegateReceipt = await delegateTx.wait();
 
   console.log(`receipt hashcode for tx is: ${delegateReceipt.transactionHash}`);
+
 }
 
 main().catch((error) => {
